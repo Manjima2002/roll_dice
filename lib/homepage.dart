ce5cd01c6931b7bb0761bd4df0eebc9d1e316503
manjima <manjima3890@gmail.com>
2024-05-24 17:13:47 +0530

    add

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:dice/diceprovide.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,24 +11,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
+  AudioPlayer audioplayer = AudioPlayer();
+  Future playAudio() async {
+    String audio = 'asseaudio/dice.mp3';
+    await audioplayer.play(AssetSource(audio));
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(child:
-              Consumer<Diceprovider>(builder: (context, dprovider, child) {
-            return GestureDetector(
-              onTap: dprovider.rollthedice,
-              child: Image.asset(
-                "asset/images/dice-${dprovider.imagenumber}.png",
-                height: 300,
-                width: 300,
-              ),
-            );
-          })),
+          Center(
+            child: Consumer<Diceprovider>(
+              builder: (context, dprovider, child) {
+                return GestureDetector(
+                  onTap: () {
+                    dprovider.rollthedice();
+                    playAudio();
+                  },
+                  child: Image.asset(
+                    "asset/images/dice-${dprovider.imagenumber}.png",
+                    height: 300,
+                    width: 300,
+                  ),
+                );
+              },
+            ),
+          ),
           SizedBox(height: 20),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => Diceprovider(),
       child: MaterialApp(
-        title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
